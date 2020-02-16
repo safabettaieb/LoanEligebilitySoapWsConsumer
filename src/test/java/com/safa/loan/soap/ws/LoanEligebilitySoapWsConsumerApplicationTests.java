e3a7c8004f8e653a49535eedaffff10c2513ecd7
safa bettieb <safa.bettaieb1@gmail.com>
2020-02-16 16:11:28 +0100

    first commit

---
+++ src/test/java/com/safa/loan/soap/ws/LoanEligebilitySoapWsConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safa.loan.soap.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanEligebilitySoapWsConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
